10554982f25b6c60f22f0b9e5c88650a408c27b9
Sefton Kimberley Downes <slvpkd@hotmail.com>
2024-03-20 09:48:45 +0000

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // reactStrictMode: false,
+  env: {
+    API_KEY: process.env.NEXT_PUBLIC_REACT_APP_OPENAI_API_KEY,
+    API_URL: process.env.NEXT_PUBLIC_REACT_APP_OPENAI_ENDPOINT,
+  },
+};
 
 export default nextConfig;
--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,30 +1,56 @@
+import OpenAI from "openai";
+
 export const fetchRequest = async (
   message: string,
   clb: (res: string) => void
 ) => {
-  await fetch(process.env.API_URL!, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
+  console.log("Fetching Story...");
+  // console.log(process.env.API_URL, process.env.API_KEY);
+
+  const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
+  async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: message }],
       model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "you are a helpful assistant",
-        },
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-    }),
-  })
-    .then((data) => data.json())
-    .then((data) => clb(data.choices[0].message.content))
+    });
+
+    return chatCompletion.choices[0].message.content;
+  }
+
+  main()
+    .then((res) => clb(res ?? ""))
     .catch((e) => {
       console.log(e);
     });
+
+  // if (process.env.API_KEY!.length > 0)
+    // await fetch("https://api.openai.com/v1/chat/completions", {
+    //   method: "POST",
+    //   headers: {
+    //     Authorization: `Bearer ${process.env.API_KEY}`,
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [
+    //       {
+    //         role: "system",
+    //         content: "you are a helpful assistant",
+    //       },
+    //       {
+    //         role: "user",
+    //         content: message,
+    //       },
+    //     ],
+    //   }),
+    // })
+    //   .then((data) => data.json())
+    //   .then((data) => clb(data.choices[0].message.content))
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
 };
